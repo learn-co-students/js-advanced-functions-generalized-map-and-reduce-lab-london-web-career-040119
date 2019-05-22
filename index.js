f1ed4f29e74121515c377d4aabcedaf7139d9d80
Ivan Szebenszki <szib77@gmail.com>
2019-05-22 22:30:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+/* eslint-disable no-extra-boolean-cast */
+
+const map = (arr, func) => {
+  const result = [];
+  arr.forEach((element) => {
+    result.push(func(element));
+  });
+  return result;
+};
+
+const reduce = (arr, func, startingValue) => {
+  let next = !!startingValue ? startingValue : arr[0];
+  const index = !!startingValue ? 0 : 1;
+
+  for (let idx = index; idx < arr.length; idx++) {
+    const element = arr[idx];
+    next = func(element, next);
+  }
+  return next;
+};
