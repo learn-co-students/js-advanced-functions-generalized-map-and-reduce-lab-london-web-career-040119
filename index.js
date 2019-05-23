ea97851356e2d4276766304ea4d858cdbada766d
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-23 10:14:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+const map = (arr, func) => {
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentElement = arr[i]
+        newArr.push(func(currentElement))
+    }
+
+    return newArr
+}
+
+function reduce(arr, callback, starting){
+    let r = (!!starting) ? starting : arr[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+      r = callback(arr[i], r)
+    }
+
+    return r;
+  } 
